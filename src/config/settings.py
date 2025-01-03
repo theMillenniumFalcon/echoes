e2c5ab6c0e77eb7725622fc425e318dfce0b7f8f
themillenniumfalcon <nishankpr2002@gmail.com>
2025-01-03 20:03:25 +0530

    added configs

---
+++ src/config/settings.py
@@ -0,0 +1,22 @@
+SPEECH_RECOGNITION_SETTINGS = {
+    'language': 'en-US',
+    'timeout': 5
+}
+
+VOICE_PROFILES = {
+    'nervous': {
+        'rate': 150,
+        'volume': 0.7,
+        'pitch': 1.2
+    },
+    'confident': {
+        'rate': 130,
+        'volume': 0.9,
+        'pitch': 1.0
+    },
+    'suspicious': {
+        'rate': 120,
+        'volume': 0.8,
+        'pitch': 0.9
+    }
+}
